61f65f19ba2d91ea430f65e0bbee4d93b46bbe74
Kevinatics <k.salgado1030@gmail.com>
2023-10-02 22:27:06 -0500

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Quiz-Time
+
+## Description
+
+The Code Quiz Game is an engaging and interactive experience that puts your coding knowledge and problem-solving skills to the test. In this game, you'll embark on a timed journey filled with coding-related questions and challenges. Your mission is to answer these questions correctly while racing against the clock.
+
+## Link
+https://github.com/Kevinatics/Quiz-Time 
+
+## Photo of the application
+
+![Alt text](<Screenshot 2023-10-02 at 9.21.40 PM.png>)
+![Alt text](<Screenshot 2023-10-02 at 10.24.24 PM.png>)
--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const timerEl = document.getElementById('timeLeft');
 const gameOverEl = document.getElementById('gameOver');
 const initialsEl = document.getElementById('initials');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const highScoreBtn = document.getElementById('highScoreBtn');
+const highScoreModal = document.getElementById('highScoreModal');
+const closeHighScoresBtn = document.getElementById('closeHighScores');
+const restartBtn = document.getElementById('restartBtn');
 
 let questions = [
     {
@@ -17,13 +21,11 @@ let questions = [
         question: "Which programming language is often used for front-end web development?",
         answers: ['Java', 'Python', 'JavaScript', 'Ruby'],
         correctAnswer: 2
-        
     },
     {
         question: "What does CSS stand for?",
         answers: ['Cascading Style Sheet', 'Computer Style Sheet', 'Creative Style Sheet', 'Colorful Style Sheet'],
         correctAnswer: 0
-        
     },
     {
         question: "In JavaScript, which keyword is used to declare a variable?",
@@ -35,10 +37,7 @@ let questions = [
         answers: ['setLocalData()', 'storeDataLocally()', 'setItem()', 'saveToLocalStorage()'],
         correctAnswer: 2
         },
-    // Added more questions here...
-];
-
-
+    ];
 let currentQuestionIndex = 0;
 let timeLeft = 100;
 let timer;
@@ -55,37 +54,36 @@ function startGame() {
 function updateTimer() {
     timeLeft--;
     timerEl.textContent = timeLeft;
-
+    document.getElementById('score').textContent = "Score: " + timeLeft;
     if (timeLeft <= 0) {
         endGame();
     }
 }
 
 function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        const currentQuestion = questions[currentQuestionIndex];
-        questionEl.textContent = currentQuestion.question;
-        answersEl.innerHTML = '';
-        currentQuestion.answers.forEach((answer, index) => {
-            const btn = document.createElement('button');
-            btn.textContent = answer;
-            btn.addEventListener('click', () => checkAnswer(index));
-            answersEl.appendChild(btn);
-        });
-    } else {
-        // User has completed all questions
-        endGame();
-    }
+    const currentQuestion = questions[currentQuestionIndex];
+    questionEl.textContent = currentQuestion.question;
+    answersEl.innerHTML = '';
+    currentQuestion.answers.forEach((answer, index) => {
+        const btn = document.createElement('button');
+        btn.textContent = answer;
+        btn.addEventListener('click', () => checkAnswer(index));
+        answersEl.appendChild(btn);
+    });
 }
 
 function checkAnswer(answerIndex) {
     if (answerIndex !== questions[currentQuestionIndex].correctAnswer) {
-        timeLeft -= 10;  // penalize for wrong answer
+        timeLeft -= 25;  // penalize for wrong answer
     }
-
+    
     currentQuestionIndex++;
 
-    displayQuestion(); // Move to the next question or end the game
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        endGame();
+    }
 }
 
 function endGame() {
@@ -101,44 +99,61 @@ function saveScore() {
     const score = timeLeft;
     localStorage.setItem('initials', initials);
     localStorage.setItem('score', score);
-    // Save initials and score to local storage 
+    // save initials and score to local storage
 }
-//highscore code
 
-let highScore = localStorage.getItem('highScore') || 0; // Load the high score from local storage or set to 0 if not available
+function saveScore() {
+    const initials = initialsEl.value;
+    const score = timeLeft;
 
+    // Retrieve the current high scores from localStorage
+    const highScores = JSON.parse(localStorage.getItem('highScores') || '[]');
+    
+    highScores.push({ initials, score });
 
+    // Sort scores in descending order and keep the top 5
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+}
+
+highScoreBtn.addEventListener('click', () => {
+    displayHighScores();
+    highScoreModal.style.display = 'block';  
+});
+
+closeHighScoresBtn.addEventListener('click', () => {
+    highScoreModal.style.display = 'none';   
+});
+
+function displayHighScores() {
+    const highScores = JSON.parse(localStorage.getItem('highScores') || '[]');
+    const highScoresList = document.getElementById('highScoresList');
+    highScoresList.innerHTML = '';
+    
+    highScores.forEach(scoreEntry => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${scoreEntry.initials}: ${scoreEntry.score}`;
+        highScoresList.appendChild(listItem);
+    });
+}
 
 function endGame() {
     clearInterval(timer);
     questionContainer.style.display = 'none';
     gameOverEl.style.display = 'block';
-
-    // Display the high score
-    document.getElementById('highScore').textContent = highScore;
+    displayHighScores();  // Displays the high scores
 }
 
-// Restart the quiz
-document.getElementById('restartBtn').addEventListener('click', () => {
-    currentQuestionIndex = 0;
-    timeLeft = 100;
-    clearInterval(timer);
-    timerEl.textContent = timeLeft;
-    startGame();
+restartBtn.addEventListener('click', () => {
+    restartGame();
 });
 
-// Save the high score
-function saveScore() {
-    const initials = initialsEl.value;
-    const score = timeLeft;
-    
-    // Check if the current score is higher than the existing high score
-    if (score > highScore) {
-        highScore = score;
-        localStorage.setItem('highScore', highScore);
-    }
-    
-    localStorage.setItem('initials', initials);
-    localStorage.setItem('score', score);
+function restartGame() {
+    currentQuestionIndex = 0;    // Reset question index
+    timeLeft = 100;              // Reset timer
+    gameOverEl.style.display = 'none';  // Hide game over screen (if shown)
+    startGame();                 // Start the game
 }
-
